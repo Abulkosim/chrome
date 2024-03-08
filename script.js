3d21e83a12b2497d4849dde0ba48577a074393c5
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2024-03-08 11:40:13 +0500

    feat: new ui

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const password = document.getElementById('password');
-const toggler = document.getElementById('toggler');
+const invisible = document.getElementById('invisible');
+const visible = document.getElementById('visible');
 
-toggler.addEventListener('click', () => {
-  if (password.type === 'password') {
-    password.type = 'text';
-  } else {
-    password.type = 'password';
-  }
+invisible.addEventListener('click', () => {
+  password.type = 'text';
+  invisible.style.display = 'none';
+  visible.style.display = 'block';
+});
+
+visible.addEventListener('click', () => {
+  password.type = 'password';
+  visible.style.display = 'none';
+  invisible.style.display = 'block';
 });
